83753b09c6e7a2a784ce1f1bd6021cbb2704bc45
tzofiyad <116486440+tzofiyad@users.noreply.github.com>
2023-05-21 01:37:10 +0300

    stage 3 with doc

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -7,24 +7,53 @@ import primitives.Point;
 import primitives.Ray;
 
 
+/**
+ * Geometries represents a collection of intersectable geometries.
+ * It implements the Intersectable interface, allowing for ray intersection tests.
+ * The class provides methods to add geometries to the collection and find intersections
+ * with a given ray.
+ *
+ * @author Tzofiya and Shira
+ */
+
 public class Geometries implements Intersectable {
     private List<Intersectable> geoList;
 
+    /**
+     * Constructs an empty Geometries object.
+     */
     public Geometries()
     {
         this.geoList = new LinkedList<Intersectable>();
     }
+
+    /**
+     * Constructs a Geometries object initialized with the given intersectable geometries.
+     *
+     * @param geometry An array of intersectable geometries to add to the collection.
+     */
     public Geometries(Intersectable...geometry) {
         geoList = new LinkedList<Intersectable>();
         Collections.addAll(geoList, geometry);
     }
+
+    /**
+     * Adds one or more intersectable geometries to the collection.
+     *
+     * @param geometries An array of intersectable geometries to add to the collection.
+     */
     public void add(Intersectable... geometries){
         Collections.addAll(geoList,geometries);//check
 
     }
-    // njnj
+
+    /**
+     * Finds the intersections between the collection of geometries and a given ray.
+     *
+     * @param ray The ray to find intersections with.
+     * @return A list of intersection points with the ray, or null if no intersections are found.
+     */
     public List<Point> findIntsersections(Ray ray) {
-                     //findIntsersections
         List<Point> result = new LinkedList<>();
         for (Intersectable geo : geoList) {
             List<Point> temp = geo.findIntsersections(ray);
