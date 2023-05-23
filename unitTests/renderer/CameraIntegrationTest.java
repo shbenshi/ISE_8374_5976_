a34a652804feee44a88961757505796b20a7aaff
tzofiyad <116486440+tzofiyad@users.noreply.github.com>
2023-05-23 13:52:37 +0300

    stage 4.8

--- unitTests/renderer/CameraIntegrationTest.java
+++ unitTests/renderer/CameraIntegrationTest.java
@@ -7,19 +7,27 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.LinkedList;
+/**
+ * This class represents a camera in a renderer.
+ *
+ * It defines the position of the camera, the direction it is facing, and the up vector.
+ * It also provides methods for setting the view plane size and distance from the camera.
+ *
+ * Authors:
+ * Tzofiya David (209918374)
+ * Shira Ben Shimol (326065976)
+ */
 
 class CameraIntegrationTest {
 
     static final Point ZERO_POINT = new Point(0, 0, 0);
 
 
-
-
     /**
      * Performs the integration between a camera and an intersectable geometry and returns the total number of intersections.
      *
-     * @param camera The camera object used for the integration.
-     * @param geometries    The intersectable geometry to perform intersection tests with.
+     * @param camera     The camera object used for the integration.
+     * @param geometries The intersectable geometry to perform intersection tests with.
      * @return The total number of intersections between the camera and the geometry.
      */
     public int cameraIntegrations(Camera camera, Intersectable geometries) {
@@ -55,16 +63,15 @@ class CameraIntegrationTest {
      * This method tests the integration between a Camera and a Sphere, calculating the number of intersections.
      */
     @Test
-    public void cameraRaySphereIntegration()
-    {
+    public void cameraRaySphereIntegration() {
         Camera camera0 = new Camera(ZERO_POINT, new Vector(0, 0, -1), new Vector(0, -1, 0));
         Camera camera1 = new Camera(new Point(0, 0, 0.5), new Vector(0, 0, -1), new Vector(0, -1, 0));
 
         // TC01: Test case for camera-ray-sphere integration
-        assertEquals(2, cameraIntegrations(camera0, new Sphere(new Point(0, 0, -3),1)));
+        assertEquals(2, cameraIntegrations(camera0, new Sphere(new Point(0, 0, -3), 1)));
 
         // TC02: Test case for camera-ray-sphere integration
-        assertEquals(18, cameraIntegrations(camera1, new Sphere(new Point(0, 0, -2.5),2.5)));
+        assertEquals(18, cameraIntegrations(camera1, new Sphere(new Point(0, 0, -2.5), 2.5)));
 
         // TC03: Test case for camera-ray-sphere integration
         assertEquals(10, cameraIntegrations(camera1, new Sphere(new Point(0, 0, -2), 2)));
@@ -81,9 +88,8 @@ class CameraIntegrationTest {
      * This method tests the integration between a Camera and a Triangle, calculating the number of intersections.
      */
     @Test
-    public void cameraRayTriangleIntegration()
-    {
-        Camera camera = new Camera(ZERO_POINT, new Vector(0,0,-1),new Vector(0,-1,0));
+    public void cameraRayTriangleIntegration() {
+        Camera camera = new Camera(ZERO_POINT, new Vector(0, 0, -1), new Vector(0, -1, 0));
 
         // TC01: Test case for camera-ray-triangle integration
         assertEquals(1, cameraIntegrations(camera, new Triangle(new Point(1, 1, -2), new Point(-1, 1, -2), new Point(0, -1, -2))));
@@ -97,9 +103,8 @@ class CameraIntegrationTest {
      * This method tests the integration between a Camera and a Plane, calculating the number of intersections.
      */
     @Test
-    public void cameraRayPlaneIntegration()
-    {
-        Camera camera = new Camera(ZERO_POINT,new Vector(0,0,-1),new Vector(0,-1,0));
+    public void cameraRayPlaneIntegration() {
+        Camera camera = new Camera(ZERO_POINT, new Vector(0, 0, -1), new Vector(0, -1, 0));
 
 
         // TC01: Test case for camera-ray-plane integration
@@ -115,76 +120,15 @@ class CameraIntegrationTest {
         assertEquals(0, cameraIntegrations(camera, new Plane(new Point(0, 0, -5), new Vector(0, -1, 0))));
 
     }
-
-
-
-
-
-
-
-
-
-
-
-    //int nX = 3;
-    //int nY = 3;
-    //view plane 3X3 (WxH 3X3 & nx,ny =3 => Rx,Ry =1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    public void cameraIntegrations(Geometry geo, Camera camera, int expected, String testCase) {
-//        List<Point> intersectionPoints;
-//        int intersections = 0;
-//        for (int i = 0; i < 3; ++i) {
-//            for (int j = 0; j < 3; ++j) {
-//                intersectionPoints = geo.findIntsersections(
-//                        camera.constructRay(3, 3, j, i));
-//                if (intersectionPoints != null)
-//                    intersections += intersectionPoints.size();
-//            }
-//        }
-//        assertEquals("ERROR " + testCase + ": Wrong amount of intersections", intersections, expected);
-//    }
 }
 
 
 
-/*
-        int counter = 0;
-        List<Point> allpoints = null;
-        cam.setVPSize(3, 3);
-        cam.setVPDistance(1);
-        int nX = 3;
-        int nY = 3;
-        //view plane 3X3 (WxH 3X3 & nx,ny =3 => Rx,Ry =1)
-        for (int i = 0; i < nY; ++i) {
-            for (int j = 0; j < nX; ++j) {
-                List<Point> intersections = geo.findIntSersections(cam.constructRay(nX, nY, j, i));
-                //adding the amount of intersections
-                if (intersections != null) {
-                    if (allpoints == null) {
-                        allpoints = new LinkedList<>();
-                    }
-                    allpoints.addAll(intersections);
-                }
-                if (intersections != null)
-                    counter += intersections.size();
-            }
-        }
-        return counter;
 
- */
+
+
+
+
+
+
+
