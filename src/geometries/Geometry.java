b23ea25eb17908e72895924e8e90b3e3689ed970
tzofiyad <116486440+tzofiyad@users.noreply.github.com>
2023-06-06 01:08:11 +0300

    6.2.8.NF

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,7 @@ package geometries;
 import primitives.Color;
 import primitives.Point;
 import primitives.Vector;
+import primitives.Material;
 
 
 /**
@@ -11,7 +12,17 @@ import primitives.Vector;
  *   and Shira ben shimol 326065976 */
 
 public abstract class Geometry extends Intersectable {
+    private Material material = new Material();
     protected Color emission = Color.BLACK;
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public Geometry setMaterial(Material material) {
+        this.material = material;
+        return this;
+    }
 
     public void setEmission(Color emission) {
         this.emission = emission;
