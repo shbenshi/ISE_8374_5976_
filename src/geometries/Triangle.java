58b022c5967b4387066c0f968f1762bfc693e3ef
shbenshi <shiraben2004@gmail.com>
2023-06-06 13:25:31 +0300

    Finish Stage 6 without documantion

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -26,7 +26,38 @@ public class Triangle extends Polygon
 
     @Override
     public List<GeoPoint> findGeoIntsersectionsHelper(Ray ray) {
-        return super.findGeoIntsersectionsHelper(ray);
+        if(this.plane.findGeoIntsersectionsHelper(ray)==null){
+            return null;
+        };
+
+        Point P0=ray.getP0();
+        Vector v=ray.getDir();
+
+        Point P1=this.vertices.get(0);
+        Point P2=this.vertices.get(1);
+        Point P3=this.vertices.get(2);
+
+        Vector v1=P1.subtract(P0);
+        Vector v2=P2.subtract(P0);
+        Vector v3=P3.subtract(P0);
+
+        double n1v=v1.crossProduct(v2).normalize().dotProduct(v);
+        double n2v=v2.crossProduct(v3).normalize().dotProduct(v);
+        double n3v=v3.crossProduct(v1).normalize().dotProduct(v);
+        if((n1v==0)||(n2v==0)||(n3v==0)){
+            return null;
+        } else if ((n1v>0)&&(n2v>0)&&(n3v>0)) {
+            Point point = plane.findGeoIntsersectionsHelper(ray).get(0).point;
+            return  List.of(new GeoPoint(this, point));
+        } else if ((n1v<0)&&(n2v<0)&&(n3v<0)) {
+            Point point = plane.findGeoIntsersectionsHelper(ray).get(0).point;
+            return  List.of(new GeoPoint(this, point));
+        }
+        else{
+            return null;
+        }
+
+
     }
 
 
