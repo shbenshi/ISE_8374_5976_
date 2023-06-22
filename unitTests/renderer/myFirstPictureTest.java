0f46c9c6b46659d62235d902f9d01b031a2c448e
tzofiyad <116486440+tzofiyad@users.noreply.github.com>
2023-06-22 21:49:08 +0300

    7.5,FP.NF

--- unitTests/renderer/myFirstPictureTest.java
+++ unitTests/renderer/myFirstPictureTest.java
@@ -24,7 +24,7 @@ public class myFirstPictureTest {
         camera.setVPSize(150, 150).setVPDistance(1000);
         scene.getGeometries().add(
 
-                new Sphere(new Point(0, 0, -50), 70).setEmission(new Color(YELLOW)) //
+                new Sphere(new Point(0, 0, -50), 65).setEmission(new Color(YELLOW)) //
                         .setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),//.setKt(0.7)),
 
                 new Sphere(new Point(32, 9, 100), 6d).setEmission(new Color(115,81,55)) //
@@ -36,9 +36,13 @@ public class myFirstPictureTest {
                 new Sphere(new Point(0, -25, 0), 20).setEmission(new Color(115,81,55)) //
                         .setMaterial(new Material().setKd(0).setKs(0).setShininess(100)),
 
-                /*new Triangle(new Point(-17,-25,0),new Point(17,-25,0), new Point(0,78,0)).setEmission(new Color(white))
-                        .setMaterial(new Material().setKd(0).setKs(0).setShininess(100)),*/
-                new Plane(new Point(0, 0, -100), new Vector(0, 0, 1)).setEmission(new Color(0, 199, 108))
+                new Triangle(new Point(-17,0,0),new Point(17,0,0), new Point(-45,65,0)).setEmission(new Color(115,81,55))
+                        .setMaterial(new Material().setKd(0).setKs(0).setShininess(100)),
+
+                new Triangle(new Point(17,0,0),new Point(-17,0,0), new Point(45,65,0)).setEmission(new Color(115,81,55))
+                        .setMaterial(new Material().setKd(0).setKs(0).setShininess(100)),
+
+                new Plane(new Point(0, 0, -100), new Vector(0, 0, 1)).setEmission(new Color(255, 197, 91))
                         .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100))
 
                 );
@@ -49,12 +53,17 @@ public class myFirstPictureTest {
                 new Sphere(new Point(0, 0, 0), 25d)
                 .setEmission(new Color(java.awt.Color.black)).setMaterial(new Material().setKd(0.25).setKs(0.5).setShininess(100)));
                         //.setKt(new Double3(0.5, 0, 0))));*/
+/*new Point(-100, -100, 500),*/
+        scene.getLights().add(
+                new SpotLight(new Color(10000, 4000, 12), new Point(0, 123, 0), new Vector(0, -2, 0))
+                        .setKl(0.00000009).setKq(0.0009));
 
+        /*scene.getLights().add(*/
         scene.getLights().add(
-                new SpotLight(new Color(10000, 60, 200), new Point(-100, -100, 500), new Vector(1, 0, -2))
-                        .setKl(0.000004).setKq(0.000006));
+                new DirectionalLight(new Color(0, 0, 120).scale(0.8), new Vector(1,67 ,0))
+        );
         scene.getLights().add(
-                new DirectionalLight(new Color(250, 150, 200).scale(0.2), new Vector(-2, -2, 1))
+                new PointLight(new Color(90,250,120),new Point( 100,100,0))
         );
 
 
