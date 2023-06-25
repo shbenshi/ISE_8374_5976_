e30c342696acfe61eb755ff12f80ad9955610006
shbenshi <shiraben2004@gmail.com>
2023-06-25 11:28:40 +0300

    minip1 Test fin

--- unitTests/renderer/minip1Test.java
+++ unitTests/renderer/minip1Test.java
@@ -16,16 +16,13 @@ import static java.awt.Color.*;
 public class minip1Test {
     @Test
     public void minip1Test() {
-        Scene scene = new Scene.SceneBuilder("finalPicture")// tzof
+        Scene scene = new Scene.SceneBuilder("finalPicture")// tzofiii
                 .build();// mens black
         Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0));
         camera.setVPSize(150, 150).setVPDistance(1000);
         scene.getGeometries().add(
         );
-        scene.getLights().add(
 
-
-        );
         camera.setImageWriter(new ImageWriter("minip1", 1000, 1000)) //
                 .setRayTracer(new RayTracerBasic(scene)) //
                 .renderImage() //
