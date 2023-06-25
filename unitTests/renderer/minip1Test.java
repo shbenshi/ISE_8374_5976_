eec86c266699f6dbc16b54132186de334d1e61bd
tzofiyad <116486440+tzofiyad@users.noreply.github.com>
2023-06-25 14:30:39 +0300

    minip 1 sun+2mountain+SceneBack

--- unitTests/renderer/minip1Test.java
+++ unitTests/renderer/minip1Test.java
@@ -17,12 +17,12 @@ public class minip1Test {
     @Test
     public void minip1Test() {
         Scene scene = new Scene.SceneBuilder("finalPicture")
-                .build();// mens black
+                .build().setSceneBack(new Color(173, 238, 255));// mens black
         Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0));
         camera.setVPSize(150, 150).setVPDistance(1000);
 
 
-
+        //sun
         scene.getGeometries().add(
                 new Sphere(new Point(-50,30,0), 10).setEmission(new Color(yellow)).
                         setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
@@ -39,21 +39,43 @@ public class minip1Test {
                 new Sphere(new Point(-47.5,42.5,0),1.7d).setEmission(new Color(yellow)).
                         setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
                 new Sphere(new Point(-43,41,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-39.8,38,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-38,34.2,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-37.6,30,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-38,26,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-40,22,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-43,19,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-47,17.3,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-51.5,17,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-56,18.3,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-59.8,20.8,0),1.7d).setEmission(new Color(yellow)).
+                        setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6)),
+                new Sphere(new Point(-61.8,25,0),1.7d).setEmission(new Color(yellow)).
                         setMaterial(new Material().setKd(0.9).setKs(0.3).setShininess(1000).setKt(0.6))
-
-
         );//shirra
 
-
-
-
-
-
+        //mountain
+        scene.getGeometries().add(
+                new Triangle(new Point(-65,-40,0),new Point(-26,13,0),new Point(0,-37,0)).setEmission(new Color(175,175,175))
+                        .setMaterial(new Material().setKd(0).setKs(0).setShininess(100)),
+                new Triangle(new Point(-45,-39,0),new Point(17,28,0),new Point(40,-37,0)).setEmission(new Color(175,175,175))
+                        .setMaterial(new Material().setKd(0).setKs(0).setShininess(100))
+        );
 
 
 
                 camera.setImageWriter(new ImageWriter("minip1", 1000, 1000)) //
-                .setRayTracer(new RayTracerBasic(scene)) //
+                 .setRayTracer(new RayTracerBasic(scene)) //
                 .renderImage() //
                 .writeToImage();
     }
